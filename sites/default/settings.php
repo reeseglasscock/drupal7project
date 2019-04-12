1c5b0f8ade9d169c113178ec646ba84eaa63c2cc
reeseglasscock <reesegpdx@gmail.com>
2019-04-12 00:02:38 -0700

    add acquia require line

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -673,3 +673,12 @@ $conf['file_scan_ignore_directories'] = array(
   'node_modules',
   'bower_components',
 );
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments
+// above.
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/reeseglasscockdrupal7/reeseglasscockdrupal7-settings.inc');
+  }
